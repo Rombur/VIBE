222beafe21baf3ba0e8a16013c9e1b01bc968f42
Bruno Turcksin <bruno.turcksin@gmail.com>
2016-11-28 18:32:28 -0500

    Increase patch.sh to fix a path used by vibe

--- core/patch.sh
+++ core/patch.sh
@@ -23,3 +23,9 @@ sed -i.old \
   -e 's|/home/batsim/caebat/vibe|/opt/vibe|' \
   -e 's|/home/batsim/caebat/data|/opt/vibe-data|' \
   /opt/vibe/examples/config/batsim.conf
+
+# Update the path of CGNS_ROOT_DIR and HDF5_ROOT
+sed -i 's|/home/batsim/caebat/install/opt/CGNS|/opt/CGNS|g' \
+  /opt/vibe/components/config/makeconfig.batsim
+sed -i 's|/home/batsim/caebat/install/opt/tpls/hdf5|/opt/tpls/hdf5|' \
+  /opt/vibe/components/config/makeconfig.batsim
