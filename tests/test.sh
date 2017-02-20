2cd0a5542d5c7dfcc84d90de93315117ef73ad0b
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-02-20 16:06:57 -0500

    Split case 7 into case7p and case7s.

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /scratch
-test_ids=( 1 2 3 6 7 10 ) 
+test_ids=( 1 2 3 6 10 ) 
 for i in "${test_ids[@]}"
 do
   echo "case ${i}"
@@ -24,3 +24,52 @@ do
   # Delete the directory that has been created
   rm -r ${case}
 done
+
+# Case 7 is particular because we run two tests in the same directory. We need
+# to clean the directory after case 7 4P
+
+case="case7"
+case_launch="./case7_launch"
+
+case_input="case7p_input"
+echo "case 7 4P"
+
+# Run the test
+cp -a ${case_input} ${case} && cd ${case} && ${case_launch}
+
+# Compare the solution
+diff TemperatureAverage.txt /opt/vibe/examples/${case}/work/battery_state/TemperatureAverage.txt > diff.txt
+cd ../
+if [ -s "${case}/diff.txt" ]
+then
+  echo "ERROR"
+  cp ${case}/diff.txt diff_7_4P.txt
+fi
+
+# Delete the directory that has been created
+rm -r ${case}
+
+# Clean the directory before 4S
+cd /opt/vibe/examples/case7
+rm batsim.conf *.log checklist.conf resource_usage RESULT
+rm -rf simulation_log simulation_results simulation_setup work
+cd /scratch
+
+
+echo "case 7 4S"
+case_input="case7s_input"
+
+# Run the test
+cp -a ${case_input} ${case} && cd ${case} && ${case_launch}
+
+# Compare the solution
+diff TemperatureAverage.txt /opt/vibe/examples/${case}/work/battery_state/TemperatureAverage.txt > diff.txt
+cd ../
+if [ -s "${case}/diff.txt" ]
+then
+  echo "ERROR"
+  cp ${case}/diff.txt diff_7_4S.txt
+fi
+
+# Delete the directory that has been created
+rm -r ${case}
